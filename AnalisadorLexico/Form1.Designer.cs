2b3396f9e5288fbf6e0b74905a186ef3128787e8
LucasCruz <lucaas-cruuz@live.com>
2022-04-11 00:44:21 -0300

    open file dialog configurado

--- AnalisadorLexico/Form1.Designer.cs
+++ AnalisadorLexico/Form1.Designer.cs
@@ -46,78 +46,74 @@ namespace AnalisadorLexico
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(446, 19);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(595, 23);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.Size = new System.Drawing.Size(119, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Analisador Léxico";
             // 
             // txtEntrada
             // 
-            this.txtEntrada.Location = new System.Drawing.Point(11, 88);
-            this.txtEntrada.Margin = new System.Windows.Forms.Padding(2);
+            this.txtEntrada.Location = new System.Drawing.Point(15, 108);
+            this.txtEntrada.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtEntrada.Name = "txtEntrada";
-            this.txtEntrada.Size = new System.Drawing.Size(408, 233);
+            this.txtEntrada.Size = new System.Drawing.Size(543, 286);
             this.txtEntrada.TabIndex = 1;
             this.txtEntrada.Text = "";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(184, 68);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(245, 84);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.Size = new System.Drawing.Size(58, 17);
             this.label2.TabIndex = 2;
             this.label2.Text = "Entrada";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(734, 68);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(979, 84);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.Size = new System.Drawing.Size(44, 17);
             this.label3.TabIndex = 4;
             this.label3.Text = "Saída";
             // 
             // txtSaida
             // 
-            this.txtSaida.Location = new System.Drawing.Point(545, 88);
-            this.txtSaida.Margin = new System.Windows.Forms.Padding(2);
+            this.txtSaida.Location = new System.Drawing.Point(727, 108);
+            this.txtSaida.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtSaida.Name = "txtSaida";
-            this.txtSaida.Size = new System.Drawing.Size(408, 233);
+            this.txtSaida.Size = new System.Drawing.Size(543, 286);
             this.txtSaida.TabIndex = 3;
             this.txtSaida.Text = "";
             // 
             // grdTblSimbolos
             // 
             this.grdTblSimbolos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.grdTblSimbolos.Location = new System.Drawing.Point(545, 386);
-            this.grdTblSimbolos.Margin = new System.Windows.Forms.Padding(2);
+            this.grdTblSimbolos.Location = new System.Drawing.Point(727, 475);
+            this.grdTblSimbolos.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.grdTblSimbolos.Name = "grdTblSimbolos";
             this.grdTblSimbolos.RowHeadersWidth = 51;
             this.grdTblSimbolos.RowTemplate.Height = 24;
-            this.grdTblSimbolos.Size = new System.Drawing.Size(408, 146);
+            this.grdTblSimbolos.Size = new System.Drawing.Size(544, 180);
             this.grdTblSimbolos.TabIndex = 5;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(702, 358);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(936, 441);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(100, 13);
+            this.label4.Size = new System.Drawing.Size(133, 17);
             this.label4.TabIndex = 6;
             this.label4.Text = "Tabela de Simbolos";
             // 
             // btnAnalisar
             // 
-            this.btnAnalisar.Location = new System.Drawing.Point(459, 95);
-            this.btnAnalisar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAnalisar.Location = new System.Drawing.Point(612, 117);
+            this.btnAnalisar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnAnalisar.Name = "btnAnalisar";
-            this.btnAnalisar.Size = new System.Drawing.Size(56, 19);
+            this.btnAnalisar.Size = new System.Drawing.Size(75, 23);
             this.btnAnalisar.TabIndex = 7;
             this.btnAnalisar.Text = "Analisar";
             this.btnAnalisar.UseVisualStyleBackColor = true;
@@ -125,10 +121,10 @@ namespace AnalisadorLexico
             // 
             // btnLimpar
             // 
-            this.btnLimpar.Location = new System.Drawing.Point(459, 137);
-            this.btnLimpar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnLimpar.Location = new System.Drawing.Point(612, 169);
+            this.btnLimpar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnLimpar.Name = "btnLimpar";
-            this.btnLimpar.Size = new System.Drawing.Size(56, 19);
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
             this.btnLimpar.TabIndex = 8;
             this.btnLimpar.Text = "Limpar";
             this.btnLimpar.UseVisualStyleBackColor = true;
@@ -140,19 +136,20 @@ namespace AnalisadorLexico
             // 
             // btnSelecionaArq
             // 
-            this.btnSelecionaArq.Location = new System.Drawing.Point(135, 342);
-            this.btnSelecionaArq.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSelecionaArq.Location = new System.Drawing.Point(180, 421);
+            this.btnSelecionaArq.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSelecionaArq.Name = "btnSelecionaArq";
-            this.btnSelecionaArq.Size = new System.Drawing.Size(113, 29);
+            this.btnSelecionaArq.Size = new System.Drawing.Size(151, 36);
             this.btnSelecionaArq.TabIndex = 9;
             this.btnSelecionaArq.Text = "Selecionar Arquivo";
             this.btnSelecionaArq.UseVisualStyleBackColor = true;
+            this.btnSelecionaArq.Click += new System.EventHandler(this.btnSelecionaArq_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1132, 567);
+            this.ClientSize = new System.Drawing.Size(1509, 698);
             this.Controls.Add(this.btnSelecionaArq);
             this.Controls.Add(this.btnLimpar);
             this.Controls.Add(this.btnAnalisar);
@@ -163,7 +160,7 @@ namespace AnalisadorLexico
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtEntrada);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Home";
             ((System.ComponentModel.ISupportInitialize)(this.grdTblSimbolos)).EndInit();
--- AnalisadorLexico/Form1.cs
+++ AnalisadorLexico/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,7 +20,7 @@ namespace AnalisadorLexico
         }
 
         private void btnAnalisar_Click(object sender, EventArgs e)
-        {
+        {          
             txtSaida.Text = scanner.Entrada(txtEntrada.Text);
             grdTblSimbolos.DataSource = scanner.identificadorList;
         }
@@ -31,6 +32,15 @@ namespace AnalisadorLexico
             grdTblSimbolos.DataSource = null;
         }
 
+        private void btnSelecionaArq_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = this.openFileDialog1.ShowDialog();
 
+            if (dr == DialogResult.OK)
+            {
+                var textFile = new StreamReader(openFileDialog1.FileName);
+                txtEntrada.Text = textFile.ReadToEnd();
+            }
+        }
     }
 }
